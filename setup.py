88d0ddac56565c90e9a9a215143032218a5ff2cf
Travis Lockman <travislockman@gmail.com>
2023-09-13 21:57:16 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="cp-hec-api-sdk",
+    version="1.0",
+    author="Travis Lockman",
+    license='Apache 2.0',
+    description="Check Point HEC API SDK",
+    long_description="Check Point Harmony Email and Collaboration SDK",
+    long_description_content_type="text/plain",
+    url="https://github.com/travislockman/cp_hec_api_python_sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+
+)
