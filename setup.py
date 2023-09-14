4ecc8e7322303bbdcd1b27b9d494e42e6e2cc705
Travis Lockman <travislockman@gmail.com>
2023-09-13 23:28:38 -0400

    Modified setup

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     long_description="Check Point Harmony Email and Collaboration SDK",
     long_description_content_type="text/plain",
     url="https://github.com/travislockman/cp_hec_api_python_sdk",
-    py_modules=["cphec.cp_hec_api"],
-    install_requires=["requests"],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
